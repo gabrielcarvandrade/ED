6510023ea66f1a2a34ad8870f9b1390471263445
Gabriel Andrade Carvalho <gabrielcarvandrade@gmail.com>
2024-03-09 14:59:12 -0300

    Update Questao28.cpp

--- Revisão - Matrizes, Ponteiros, Registros e Arquivos Tipados e em Modo Texto/Questão 28: Ponteiros - Subtração de vetores de caractere/Questao28.cpp
+++ Revisão - Matrizes, Ponteiros, Registros e Arquivos Tipados e em Modo Texto/Questão 28: Ponteiros - Subtração de vetores de caractere/Questao28.cpp
@@ -34,23 +34,23 @@ using namespace std;
 
 int main()
 {
-    int n1, n2;
-    cin >> n1;
-    char *vet1 = new char[n1];
-    for (int i = 0; i < n1; i++)
+    int numVet1, numVet2;
+    cin >> numVet1;
+    char *vet1 = new char[numVet1];
+    for (int i = 0; i < numVet1; i++)
     {
         cin >> vet1[i];
     }
-    cin >> n2;
-    char *vet2 = new char[n2];
-    for (int i = 0; i < n2; i++)
+    cin >> numVet2;
+    char *vet2 = new char[numVet2];
+    for (int i = 0; i < numVet2; i++)
     {
         cin >> vet2[i];
     }
     int cont = 0;
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < numVet1; i++)
     {
-        for (int j = 0; j < n2; j++)
+        for (int j = 0; j < numVet2; j++)
         {
             if (vet1[i] == vet2[j])
             {
@@ -59,9 +59,9 @@ int main()
             }
         }
     }
-    char *vet3 = new char[n1 - cont];
+    char *vet3 = new char[numVet1 - cont];
     int k = 0;
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < numVet1; i++)
     {
         if (vet1[i] != ' ')
         {
@@ -69,7 +69,7 @@ int main()
             k++;
         }
     }
-    for (int i = 0; i < n1 - cont; i++)
+    for (int i = 0; i < numVet1 - cont; i++)
     {
         cout << vet3[i] << " ";
     }
